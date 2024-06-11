174c42698c81b0f0b4daeefac3fea58079369943
Charles <contact@charles-mariot.fr>
2024-06-11 15:51:08 +0200

    Test tutorial

--- srcs/main.py
+++ srcs/main.py
@@ -6,7 +6,7 @@
 #    By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/10 10:19:26 by cmariot           #+#    #+#              #
-#    Updated: 2024/06/11 10:09:54 by cmariot          ###   ########.fr        #
+#    Updated: 2024/06/11 14:06:07 by cmariot          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,13 +19,22 @@ from preprocessing_parsing_formating.plot import plot
 from preprocessing_parsing_formating.dimensionality_reduction import dimensionality_reduction
 
 
+def print_title(title: str):
+    """
+    Print a title
+    """
+    BLUE = "\033[94m"
+    END = "\033[0m"
+
+    print("\n" + "=" * 80)
+    print(BLUE + title + END)
+    print("=" * 80 + "\n")
+
+
 if __name__ == "__main__":
     try:
         for patient_id in range(1, 110):
             for recording_id in range(1, 15):
-
-                # patient_id = 1
-                # recording_id = 4
 
                 if patient_id == 100 and recording_id > 2:
                     # Patient 100 Fc5. channel is not connected ?
@@ -37,31 +46,50 @@ if __name__ == "__main__":
                 print(f"Patient ID: {patient_id} - Recording ID: {recording_id}")
 
                 # Load and visualize the raw data
+                print_title("Loading and visualizing the raw data")
                 raw: mne.io.BaseRaw = open_raw_data(patient_id, recording_id)
                 # raw = plot(raw, patient_id, recording_id)
 
                 # Apply a filter to the data (remove noise)
                 filtered: mne.io.BaseRaw = filter_data(
-                    raw, low_freq=1.0, high_freq=40.0
+                    raw, low_freq=8.0, high_freq=32.0
                 )
-                filtered = plot(
-                    filtered, patient_id, recording_id, is_filtered=True
-                )
+                # filtered = raw
+                # filtered = plot(
+                #     filtered, patient_id, recording_id, is_filtered=True
+                # )
+
+                print(filtered)
+                print(filtered.info)
 
                 # Compute the power spectral density (PSD) of the data
                 spectrum: mne.time_frequency.Spectrum = filtered.compute_psd(
                     method="welch",
-                    # fmin=1,
-                    # fmax=40.0
-                    proj=True
+                    fmin=0,
+                    fmax=80.0,
                 )
                 spectrum.plot(
                     average=True,
                     amplitude=False,
                     dB=True,
                 )
-                # plt.show()
+                plt.show()
 
+                # events
+                events, event_dict = mne.events_from_annotations(
+                    filtered, verbose=False
+                )
+
+                print(events)
+
+                epochs = mne.Epochs(
+                    filtered,
+                    events,
+                    event_dict,
+                    baseline=None,
+                )
+
+                print(epochs)
                 # Number of features = number of electrodes (64)
 
                 # Apply a Dimensionality reduction algorithm
@@ -71,9 +99,9 @@ if __name__ == "__main__":
 
                 # x.shape = (64, 1025)
 
-                x = dimensionality_reduction(
-                    x=spectrum.get_data()
-                )
+                # x = dimensionality_reduction(
+                #     x=spectrum.get_data()
+                # )
 
                 raw.close()
                 filtered.close()
---
+++ srcs/test.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+from sklearn.pipeline import Pipeline
+
+from mne import Epochs, pick_types
+from mne.channels import make_standard_montage
+from mne.datasets import eegbci
+from mne.decoding import CSP
+from mne.io import concatenate_raws
+from mne.time_frequency import Spectrum
+from preprocessing_parsing_formating.open import open_raw_data
+
+
+if __name__ == "__main__":
+
+    # Tutorial :
+    # https://mne.tools/dev/auto_examples/decoding/decoding_csp_eeg.html
+
+    subject = 1
+    runs = [3, 4, 7, 8, 11, 12]
+
+    raw = concatenate_raws([open_raw_data(subject, run) for run in runs])
+
+    eegbci.standardize(raw)
+    montage = make_standard_montage("standard_1005")
+    raw.set_montage(montage)
+    raw.set_eeg_reference(projection=True)
+
+    filtered = raw.filter(
+        8.0, 32.0, fir_design="firwin", skip_by_annotation="edge"
+    )
+
+    # Compute the power spectral density (PSD) of the data
+    spectrum: Spectrum = filtered.compute_psd(
+        method="welch",
+        fmin=0,
+        fmax=80.0,
+    )
+    spectrum.plot(
+        average=True,
+        amplitude=False,
+        dB=True,
+    )
+    plt.show()
+
+    tmin, tmax = -1.0, 5.0
+    picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads")
+
+    epochs = Epochs(
+        raw,
+        event_id=['T0: rest', 'T1: left fist', 'T2: right fist'],
+        tmin=tmin,
+        tmax=tmax,
+        proj=True,
+        picks=picks,
+        baseline=None,
+        preload=True,
+    )
+
+    epochs_train = epochs.copy().crop(tmin=1.0, tmax=2.0)
+    labels = epochs.events[:, -1] - 2
+
+    # Define a monte-carlo cross-validation generator (reduce variance):
+    scores = []
+    epochs_data = epochs.get_data(copy=False)
+    epochs_data_train = epochs_train.get_data(copy=False)
+    cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+    cv_split = cv.split(epochs_data_train)
+
+    # Assemble a classifier
+    lda = LinearDiscriminantAnalysis()
+    csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
+
+    # Use scikit-learn Pipeline with cross_val_score function
+    clf = Pipeline([("CSP", csp), ("LDA", lda)])
+    scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=None)
+
+    # Printing the results
+    class_balance = np.mean(labels == labels[0])
+    class_balance = max(class_balance, 1.0 - class_balance)
+
+    print("\n" + "=" * 80 + "\n")
+    print(f"Classification accuracy: {np.mean(scores)} / Chance level: {class_balance}")
+    print("\n" + "=" * 80 + "\n")
+
+    # plot CSP patterns estimated on full data for visualization
+    csp.fit_transform(epochs_data, labels)
+
+    csp.plot_patterns(epochs.info, ch_type="eeg", units="Patterns (AU)", size=1.5)
+    plt.show()
+
+    # ************************ #
+    # Classification over time #
+    # ************************ #
+
+    sfreq = raw.info["sfreq"]
+    w_length = int(sfreq * 0.5)  # running classifier: window length
+    w_step = int(sfreq * 0.1)  # running classifier: window step size
+    w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
+
+    scores_windows = []
+
+    for train_idx, test_idx in cv_split:
+        y_train, y_test = labels[train_idx], labels[test_idx]
+
+        X_train = csp.fit_transform(epochs_data_train[train_idx], y_train)
+        X_test = csp.transform(epochs_data_train[test_idx])
+
+        # fit classifier
+        lda.fit(X_train, y_train)
+
+        # running classifier: test classifier on sliding window
+        score_this_window = []
+        for n in w_start:
+            X_test = csp.transform(epochs_data[test_idx][:, :, n : (n + w_length)])
+            score_this_window.append(lda.score(X_test, y_test))
+        scores_windows.append(score_this_window)
+
+    # Plot scores over time
+    w_times = (w_start + w_length / 2.0) / sfreq + epochs.tmin
+
+    plt.figure()
+    plt.plot(w_times, np.mean(scores_windows, 0), label="Score")
+    plt.axvline(0, linestyle="--", color="k", label="Onset")
+    plt.axhline(0.5, linestyle="-", color="k", label="Chance")
+    plt.xlabel("time (s)")
+    plt.ylabel("classification accuracy")
+    plt.title("Classification score over time")
+    plt.legend(loc="lower right")
+    plt.show()
